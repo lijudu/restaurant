538261f73ac602750ac25c2f1312f3fbbcf5e900
Lisa <mkoliju@gmail.com>
2022-11-11 09:13:59 -0800

    Fix background opactiy

--- src/index.js
+++ src/index.js
@@ -2,28 +2,30 @@ import catImg from './assets/zhang-kaiyv-unsplash.jpg'
 import './styles.css'
 
 const index = document.getElementById('index')
+const backImg = document.createElement('div')
 const cafeTitle = document.createElement('div')
 const topMenu = document.createElement('div')
 const topList = document.createElement('LI')
 const topHome = document.createElement('LI')
 const topContact = document.createElement('LI')
 
-
-index.style.backgroundImage = "url('/Users/Lisa/Desktop/repos/restaurant/dist/d47c346547c960bf39cab6e6088d2755.jpg')"
-
 cafeTitle.id = 'title'
 topMenu.id = 'bar'
+backImg.id = 'catImg'
 
 cafeTitle.innerText = 'Cat Cat Catfe'
 topList.innerText = 'Menu'
 topHome.innerText = 'Home'
 topContact.innerText ='Contact'
 
-
+index.appendChild(backImg)
 index.appendChild(topMenu)
 index.appendChild(cafeTitle)
 
 const bar = document.getElementById('bar')
+const image = document.getElementById('catImg')
+
+image.style.backgroundImage = "url('/Users/Lisa/Desktop/repos/restaurant/dist/d47c346547c960bf39cab6e6088d2755.jpg')"
 
 bar.appendChild(topHome)
 bar.appendChild(topList)
